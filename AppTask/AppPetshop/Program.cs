20f70676217d64af45fc33a87e842140d6671708
kahhhh666 <kauehenriquelima23@gmail.com>
2026-09-18 09:11:41 -0300

    Atualiza no program.cs para permitir a conexão com o banco de dados

--- AppTask/AppPetshop/Program.cs
+++ AppTask/AppPetshop/Program.cs
@@ -1,7 +1,12 @@
+using AppPetshop.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PetshopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSqlServer")));
 
 var app = builder.Build();
 
